1d1bf94d478821c6faa93ee928347819e895e584
Dcyaprogrammer <dujustin6@gmail.com>
2025-11-07 20:44:20 +0800

    basic render

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,3 @@
-import {
-  ASCIIFont,
-  Box,
-  createCliRenderer,
-  Text,
-  TextAttributes,
-} from "@opentui/core";
+import { startUi } from './ui/app';
 
-const renderer = await createCliRenderer({ exitOnCtrlC: true });
-
-renderer.root.add(
-  Box(
-    { alignItems: "center", justifyContent: "center", flexGrow: 1 },
-    Box(
-      { justifyContent: "center", alignItems: "flex-end" },
-      ASCIIFont({ font: "tiny", text: "OpenTUI" }),
-      Text({ content: "What will you build?", attributes: TextAttributes.DIM }),
-    ),
-  ),
-);
+await startUi();
